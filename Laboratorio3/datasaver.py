e3be67681af4432ff14a177c113f3f9fcd038403
LuisNuMo7 <luisdiego.nuez@yahoo.com>
2024-05-07 23:46:21 -0600

    Script de python para comunicacion serial

---
+++ Laboratorio3/datasaver.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import serial
+
+ser = serial.Serial(
+    port = '/tmp/ttyS1',\
+#    port = 'COM3',\
+    baudrate = 9600,\
+    parity=serial.PARITY_NONE,\
+    stopbits=serial.STOPBITS_ONE,\
+    bytesize=serial.EIGHTBITS,
+    timeout=0\
+    )
+#ser=serial.Serial('/tmp/ttyS1', 9600)
+#f = open('Tensiones.csv', 'w+')
+
+print("connected to: "+ ser.portstr)
+
+try:
+    while True:
+        for c in ser.read():
+            c=chr(c)
+            print(c, end= "")
+ #           f.write(c)
+except KeyboardInterrupt:
+    print("\nInterrupción de Programa debido al usuario.")
+
+finally:
+    ser.close()
+  #  f.close()
+
+
