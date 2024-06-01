4a24c1305962f4d6340edab2fc493fcce2b3ff53
Seth Harrington <sbgha.09@gmail.com>
2024-05-31 21:00:33 -0600

    Ya esta listo el script de python para leer los datos de la placa

--- Laboratorio4/mqtt-thingsboard.py
+++ Laboratorio4/mqtt-thingsboard.py
@@ -1,42 +1,55 @@
 from tb_device_mqtt import TBDeviceMqttClient, TBPublishInfo
 import serial
+import time
 
 broker="iot.eie.ucr.ac.cr"
 topic="v1/devices/me/telemetry"
 password="YpucCrCnikzUd5W0tQnm"
+angle_limit = 35
+## Serial data preparation
+ser = serial.Serial(
+    port = '/tmp/ttyS1',\
+#    port = 'COM3',\
+    baudrate = 9600,\
+    parity=serial.PARITY_NONE,\
+    stopbits=serial.STOPBITS_ONE,\
+    bytesize=serial.EIGHTBITS,
+    timeout=0\
+    )
 
-# ## Serial data preparation
-# ser = serial.Serial(
-#     port = '/tmp/ttyS1',\
-# #    port = 'COM3',\
-#     baudrate = 9600,\
-#     parity=serial.PARITY_NONE,\
-#     stopbits=serial.STOPBITS_ONE,\
-#     bytesize=serial.EIGHTBITS,
-#     timeout=0\
-#     )
-# try:
-#     while True:
-#         for c in ser.read():
-#             c=chr(c)
-#             print(c, end= "")
-# except KeyboardInterrupt:
-#     print("\nInterrupción de Programa debido al usuario.")
-
-
-
-
-telemetry = { "valuex":1,"valuey":2,"valuez":3, "enabled": False}
-
-client = TBDeviceMqttClient(broker, username=password)
 # Connect to ThingsBoard
+client = TBDeviceMqttClient(broker, username=password)
 client.connect()
-# Sending telemetry without checking the delivery status
-client.send_telemetry(telemetry) 
-# Sending telemetry and checking the delivery status (QoS = 1 by default)
-result = client.send_telemetry(telemetry)
-# get is a blocking call that awaits delivery status  
-success = result.get() == TBPublishInfo.TB_ERR_SUCCESS
+try:
+    while True:
+        #Preparing data to send
+        h = ""
+        while not(h.endswith("\r\n")):
+            for c in ser.read():
+                c=chr(c)
+                h+= str(c)
+        eje_x, eje_y, eje_z, temperatura, bateria, comunicacion = h.split(",")
+        eje_x, eje_y, eje_z, temperatura, bateria , comunicacion= float(eje_x), float(eje_y), float(eje_z), float(temperatura), float(bateria), bool(comunicacion)
+        ang_deformacion = (eje_x >= angle_limit or eje_y >= angle_limit or eje_z >= angle_limit)
+        # Wraping data
+        telemetry = { "Eje X":eje_x,\
+                        "Eje Y":eje_y,\
+                        "Eje Z":eje_z,\
+                        "Temperatura":temperatura,\
+                        "Bateria":bateria,\
+                        "Comunicacion": comunicacion,\
+                        "Deformacion angular":ang_deformacion}
+        
+        # Sending telemetry and checking the delivery status (QoS = 1 by default)
+        result = client.send_telemetry(telemetry)
+        
+        # get is a blocking call that awaits delivery status  
+        success = result.get() == TBPublishInfo.TB_ERR_SUCCESS
+        
+        # print(result.get())
+        print(h)
+        time.sleep(5)
+except KeyboardInterrupt:
+    print("\nInterrupción de Programa debido al usuario.")
 # Disconnect from ThingsBoard
 client.disconnect()
-
