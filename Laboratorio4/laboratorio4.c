b3b90c7e3722c122d8eb4e27e631e6a342e3a3eb
Seth Harrington <sbgha.09@gmail.com>
2024-06-05 08:54:12 -0600

    Modelo del carro en arduino Servo solo gira 90 grados

--- Laboratorio4/laboratorio4.c
+++ Laboratorio4/laboratorio4.c
@@ -43,9 +43,6 @@ char console_getc(int wait);
 void console_puts(char *s);
 int console_gets(char *s, int len);
 
-float bateria = 0.95;
-float temperatura = 25.00;
-bool comunicacion = false;
 static void clock_setup_enable(void)
 {
 	rcc_clock_setup_pll(&rcc_hse_8mhz_3v3[RCC_CLOCK_3V3_84MHZ]);
@@ -127,236 +124,171 @@ static void print_int(uint32_t usart, int32_t valor)
 
 int main(void)
 {
-#include <stdio.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
+	// Configuracion de la bateria externa
+	gpio_mode_setup(GPIOE, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, GPIO4);
+	gpio_clear(GPIOE, GPIO4);
+	// Configuracion del boton
+	gpio_mode_setup(GPIOA, GPIO_MODE_INPUT, GPIO_PUPD_NONE, GPIO0);
+	// Configuracion del led
+	gpio_mode_setup(GPIOG, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO13 | GPIO14);
 
-#define PORT 1716
+	clock_setup();
+	sdram_init();
+	clock_setup_enable();
+	usart_setup();
+	gyro_setup();
+	lcd_spi_init();
 
-	int main()
+	// Configuración inicial del giroscopio:
+	gpio_clear(GPIOC, GPIO1);
+	spi_send(SPI5, CTRL_REG1);
+	spi_read(SPI5);
+	// Habilitación de Ejes
+	spi_send(SPI5, CTRL_REG1_PD | CTRL_REG1_XEN |
+					   CTRL_REG1_YEN | CTRL_REG1_ZEN |
+					   (3 << CTRL_REG1_BW_SHIFT));
+	spi_read(SPI5);
+	gpio_set(GPIOC, GPIO1);
+
+	gpio_clear(GPIOC, GPIO1);
+	// Prescaler
+	spi_send(SPI5, CTRL_REG2);
+	spi_read(SPI5);
+	spi_send(SPI5, ~CTRL_REG2_HPM1 | CTRL_REG2_HPM2 | CTRL_REG2_HPCF0 | CTRL_REG2_HPCF1 | CTRL_REG2_HPCF2 | CTRL_REG2_HPCF3);
+	spi_read(SPI5);
+	gpio_set(GPIOC, GPIO1);
+	gpio_clear(GPIOC, GPIO1);
+	spi_send(SPI5, CTRL_REG4);
+	spi_read(SPI5);
+	spi_send(SPI5, (1 << CTRL_REG4_FS_SHIFT));
+	spi_read(SPI5);
+	// Configuración inicial de la pantalla:
+	gfx_init(lcd_draw_pixel, 240, 320);
+	gfx_fillScreen(LCD_GREY);
+	gfx_fillRoundRect(10, 10, 220, 220, 5, LCD_WHITE);
+	gfx_drawRoundRect(10, 10, 220, 220, 5, LCD_RED);
+	gfx_setTextSize(2);
+	gfx_setCursor(15, 25);
+	gfx_puts("MONITOR");
+	gfx_setTextSize(2);
+	gfx_setCursor(15, 60);
+	gfx_puts("DE PENDIENTES");
+	gfx_setCursor(15, 80);
+	lcd_show_frame(); // permite mostrar diseño en pantalla
+	// Variables para obtencion de datos, conversion e impresion en pantalla
+	int16_t giro_x, giro_y, giro_z;
+	char giro_x_char[15], giro_y_char[15], giro_z_char[15];
+	int boton_com_enable;
+	boton_com_enable = 0;
+	while (1)
 	{
-		///////////////////////////////////////////
-		//             ABRIENDO SOCKET           //
-		///////////////////////////////////////////
-		int sockfd, newsockfd;
-		struct sockaddr_in addr, cliaddr;
-		socklen_t clilen;
-		char buffer[1024];
 
-		// Crear un socket
-		sockfd = socket(AF_INET, SOCK_STREAM, 0);
-		if (sockfd < 0)
-		{
-			perror("socket");
-			exit(1);
-		}
+		// Configuración de Pantalla Principal:
 
-		// Configurar el socket
-		addr.sin_family = AF_INET;
-		addr.sin_addr.s_addr = INADDR_ANY;
-		addr.sin_port = htons(PORT);
-
-		// Asociar el socket a una dirección local
-		if (bind(sockfd, (struct sockaddr *)&addr, sizeof(addr)) < 0)
-		{
-			perror("bind");
-			exit(1);
-		}
-
-		// Escuchar en el socket
-		if (listen(sockfd, 5) < 0)
-		{
-			perror("listen");
-			exit(1);
-		}
-
-		// Aceptar una conexión
-		clilen = sizeof(cliaddr);
-		newsockfd = accept(sockfd, (struct sockaddr *)&cliaddr, &clilen);
-		if (newsockfd < 0)
-		{
-			perror("accept");
-			exit(1);
-		}
-
-		// Configuracion de la bateria externa
-		gpio_mode_setup(GPIOE, GPIO_MODE_ANALOG, GPIO_PUPD_NONE, GPIO4);
-		gpio_clear(GPIOE, GPIO4);
-		// Configuracion del boton
-		gpio_mode_setup(GPIOA, GPIO_MODE_INPUT, GPIO_PUPD_NONE, GPIO0);
-		// Configuracion del led
-		gpio_mode_setup(GPIOG, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO13 | GPIO14);
-
-		clock_setup();
-		sdram_init();
-		clock_setup_enable();
-		usart_setup();
-		gyro_setup();
-		lcd_spi_init();
-
-		// Configuración inicial del giroscopio:
-		gpio_clear(GPIOC, GPIO1);
-		spi_send(SPI5, CTRL_REG1);
-		spi_read(SPI5);
-		// Habilitación de Ejes
-		spi_send(SPI5, CTRL_REG1_PD | CTRL_REG1_XEN |
-						   CTRL_REG1_YEN | CTRL_REG1_ZEN |
-						   (3 << CTRL_REG1_BW_SHIFT));
-		spi_read(SPI5);
-		gpio_set(GPIOC, GPIO1);
-
-		gpio_clear(GPIOC, GPIO1);
-		// Prescaler
-		spi_send(SPI5, CTRL_REG2);
-		spi_read(SPI5);
-		spi_send(SPI5, ~CTRL_REG2_HPM1 | CTRL_REG2_HPM2 | CTRL_REG2_HPCF0 | CTRL_REG2_HPCF1 | CTRL_REG2_HPCF2 | CTRL_REG2_HPCF3);
-		spi_read(SPI5);
-		gpio_set(GPIOC, GPIO1);
-		gpio_clear(GPIOC, GPIO1);
-		spi_send(SPI5, CTRL_REG4);
-		spi_read(SPI5);
-		spi_send(SPI5, (1 << CTRL_REG4_FS_SHIFT));
-		spi_read(SPI5);
-		// Configuración inicial de la pantalla:
-		gfx_init(lcd_draw_pixel, 240, 320);
 		gfx_fillScreen(LCD_GREY);
 		gfx_fillRoundRect(10, 10, 220, 220, 5, LCD_WHITE);
 		gfx_drawRoundRect(10, 10, 220, 220, 5, LCD_RED);
-		gfx_setTextSize(2);
-		gfx_setCursor(15, 25);
-		gfx_puts("MONITOR");
-		gfx_setTextSize(2);
-		gfx_setCursor(15, 60);
-		gfx_puts("DE PENDIENTES");
+		gfx_setCursor(150, 18.5);
+		gfx_drawRoundRect(190, 15, 30, 15, 4, LCD_BLACK);
+		gfx_drawRect(220, 18.9, 2, 7, LCD_BLACK);
+		gfx_setTextSize(1.5);
+		gfx_setCursor(15, 40);
+		gfx_puts("Monitor de pendientes");
+		gfx_setTextSize(0.9);
+
+		// Toma de datos del giroscopio:
+
+		// Eje x:
+		gpio_clear(GPIOC, GPIO1);	   // poner pin en bajo
+		spi_send(SPI5, OUT_X_L | RNW); // enviar comando para lectura
+		spi_read(SPI5);				   // Lectura del bus SPI
+		spi_send(SPI5, 0);			   // Inicia la transferencia de datos del giroscopio al microcontrolador.
+		giro_x = spi_read(SPI5);	   // Lectura y almacenamiento del valor del giro
+		gpio_set(GPIOC, GPIO1);		   // poner pin en alto, termina comunicacion temporal
+
+		gpio_clear(GPIOC, GPIO1);	   // Inicia el ciclo nuevamente
+		spi_send(SPI5, OUT_X_H | RNW); // enviar comando para lectura
+		spi_read(SPI5);				   // Lectura del bus SPI
+		spi_send(SPI5, 0);			   // Inicia la transferencia de datos del giroscopio al microcontrolador.
+		giro_x |= spi_read(SPI5) << 8; // Se lee el dato obtenido y se combina con el anterior obtenido
+		gfx_setTextSize(1);			   // Tamaño del texto
 		gfx_setCursor(15, 80);
-		lcd_show_frame(); // permite mostrar diseño en pantalla
-		// Variables para obtencion de datos, conversion e impresion en pantalla
-		int16_t giro_x, giro_y, giro_z;
-		char giro_x_char[15], giro_y_char[15], giro_z_char[15];
-		int boton_com_enable;
-		boton_com_enable = 0;
-		while (1)
+		gfx_puts("Eje x: ");
+		sprintf(giro_x_char, "%d", giro_x); // Conversion a caracteres del valor obtenido del giro y almacenamiento en giro_x_char
+		gfx_puts(giro_x_char);
+		gpio_set(GPIOC, GPIO1); // Pone pin en alto, fin de la comunicacion
+
+		// Eje y:
+		gpio_clear(GPIOC, GPIO1);
+		spi_send(SPI5, OUT_Y_L | RNW);
+		spi_read(SPI5);
+		spi_send(SPI5, 0);
+		giro_y = spi_read(SPI5);
+		gpio_set(GPIOC, GPIO1);
+
+		gpio_clear(GPIOC, GPIO1);
+		spi_send(SPI5, OUT_Y_H | RNW);
+		spi_read(SPI5);
+		spi_send(SPI5, 0);
+		giro_y |= spi_read(SPI5) << 8;
+		gfx_setCursor(15, 100);
+		gfx_puts("Eje y: ");
+		sprintf(giro_y_char, "%d", giro_y);
+		gfx_puts(giro_y_char);
+		gpio_set(GPIOC, GPIO1);
+
+		// Eje z:
+		gpio_clear(GPIOC, GPIO1);
+		spi_send(SPI5, OUT_Z_L | RNW);
+		spi_read(SPI5);
+		spi_send(SPI5, 0);
+		giro_z = spi_read(SPI5);
+		gpio_set(GPIOC, GPIO1);
+
+		gpio_clear(GPIOC, GPIO1);
+		spi_send(SPI5, OUT_Z_H | RNW);
+		spi_read(SPI5);
+		spi_send(SPI5, 0);
+		giro_z |= spi_read(SPI5) << 8;
+		gfx_setCursor(15, 120);
+		gfx_puts("Eje z: ");
+		sprintf(giro_z_char, "%d", giro_z);
+		gfx_puts(giro_z_char);
+		gfx_setTextSize(1.5);
+		gfx_setCursor(15, 140);
+		gfx_puts("Comunicacion: ");
+		gpio_get(GPIOA, GPIO0);
+		// Funcionamiento del boton y habilitación de USART:
+		if (gpio_get(GPIOA, GPIO0) & (boton_com_enable == 0))
 		{
-
-			// Configuración de Pantalla Principal:
-
-			gfx_fillScreen(LCD_GREY);
-			gfx_fillRoundRect(10, 10, 220, 220, 5, LCD_WHITE);
-			gfx_drawRoundRect(10, 10, 220, 220, 5, LCD_RED);
-			gfx_setCursor(150, 18.5);
-			gfx_drawRoundRect(190, 15, 30, 15, 4, LCD_BLACK);
-			gfx_drawRect(220, 18.9, 2, 7, LCD_BLACK);
-			gfx_setTextSize(1.5);
-			gfx_setCursor(15, 40);
-			gfx_puts("Monitor de pendientes");
-			gfx_setTextSize(0.9);
-
-			// Toma de datos del giroscopio:
-
-			// Eje x:
-			gpio_clear(GPIOC, GPIO1);	   // poner pin en bajo
-			spi_send(SPI5, OUT_X_L | RNW); // enviar comando para lectura
-			spi_read(SPI5);				   // Lectura del bus SPI
-			spi_send(SPI5, 0);			   // Inicia la transferencia de datos del giroscopio al microcontrolador.
-			giro_x = spi_read(SPI5);	   // Lectura y almacenamiento del valor del giro
-			gpio_set(GPIOC, GPIO1);		   // poner pin en alto, termina comunicacion temporal
-
-			gpio_clear(GPIOC, GPIO1);	   // Inicia el ciclo nuevamente
-			spi_send(SPI5, OUT_X_H | RNW); // enviar comando para lectura
-			spi_read(SPI5);				   // Lectura del bus SPI
-			spi_send(SPI5, 0);			   // Inicia la transferencia de datos del giroscopio al microcontrolador.
-			giro_x |= spi_read(SPI5) << 8; // Se lee el dato obtenido y se combina con el anterior obtenido
-			gfx_setTextSize(1);			   // Tamaño del texto
-			gfx_setCursor(15, 80);
-			gfx_puts("Eje x: ");
-			sprintf(giro_x_char, "%d", giro_x); // Conversion a caracteres del valor obtenido del giro y almacenamiento en giro_x_char
-			gfx_puts(giro_x_char);
-			gpio_set(GPIOC, GPIO1); // Pone pin en alto, fin de la comunicacion
-
-			// Eje y:
-			gpio_clear(GPIOC, GPIO1);
-			spi_send(SPI5, OUT_Y_L | RNW);
-			spi_read(SPI5);
-			spi_send(SPI5, 0);
-			giro_y = spi_read(SPI5);
-			gpio_set(GPIOC, GPIO1);
-
-			gpio_clear(GPIOC, GPIO1);
-			spi_send(SPI5, OUT_Y_H | RNW);
-			spi_read(SPI5);
-			spi_send(SPI5, 0);
-			giro_y |= spi_read(SPI5) << 8;
-			gfx_setCursor(15, 100);
-			gfx_puts("Eje y: ");
-			sprintf(giro_y_char, "%d", giro_y);
-			gfx_puts(giro_y_char);
-			gpio_set(GPIOC, GPIO1);
-
-			// Eje z:
-			gpio_clear(GPIOC, GPIO1);
-			spi_send(SPI5, OUT_Z_L | RNW);
-			spi_read(SPI5);
-			spi_send(SPI5, 0);
-			giro_z = spi_read(SPI5);
-			gpio_set(GPIOC, GPIO1);
-
-			gpio_clear(GPIOC, GPIO1);
-			spi_send(SPI5, OUT_Z_H | RNW);
-			spi_read(SPI5);
-			spi_send(SPI5, 0);
-			giro_z |= spi_read(SPI5) << 8;
-			gfx_setCursor(15, 120);
-			gfx_puts("Eje z: ");
-			sprintf(giro_z_char, "%d", giro_z);
-			gfx_puts(giro_z_char);
-			gfx_setTextSize(1.5);
-			gfx_setCursor(15, 140);
-			gfx_puts("Comunicacion: ");
-			gpio_get(GPIOA, GPIO0);
-			// Funcionamiento del boton y habilitación de USART:
-			if (gpio_get(GPIOA, GPIO0) & (boton_com_enable == 0))
-			{
-				boton_com_enable = 1;
-			}
-			else if (gpio_get(GPIOA, GPIO0) & (boton_com_enable == 1))
-			{
-				boton_com_enable = 0;
-			}
-			if (boton_com_enable == 1)
-			{
-				gfx_puts("ON");
-				gpio_toggle(GPIOG, GPIO13);
-				// Eje x:
-				print_int(CONSOLE_UART, giro_x);
-				console_puts(" \n");
-				// Eje y:
-				print_int(CONSOLE_UART, giro_y);
-				console_puts(" \n");
-				// Eje z:
-				print_int(CONSOLE_UART, giro_z);
-				console_puts(" \n");
-				gpio_set(GPIOC, GPIO1);
-			}
-			else if (boton_com_enable == 0)
-			{
-				gfx_puts("OFF");
-				gpio_clear(GPIOG, GPIO13);
-			}
-			gfx_puts("Temperatura:");
-			lcd_show_frame();
-			// Enviar un mensaje
-			sprintf(message, "%f,%f,%f,%f,%f,%s", giro_x, giro_y, giro_z, bateria, temperatura, comunicacion ? "True\r\n" : "False\r\n");
-			int n = send(newsockfd, message, strlen(message), 0);
-			if (n < 0)
-			{
-				perror("send");
-				exit(1);
-			}
+			boton_com_enable = 1;
 		}
-		///////////////////////////////////////////
-		//             CERRANDO SOCKET           //
-		///////////////////////////////////////////
-		close(newsockfd);
-		close(sockfd);
-		return 0;
+		else if (gpio_get(GPIOA, GPIO0) & (boton_com_enable == 1))
+		{
+			boton_com_enable = 0;
+		}
+		if (boton_com_enable == 1)
+		{
+			gfx_puts("ON");
+			gpio_toggle(GPIOG, GPIO13);
+			// Eje x:
+			print_int(CONSOLE_UART, giro_x);
+			console_puts(" \n");
+			// Eje y:
+			print_int(CONSOLE_UART, giro_y);
+			console_puts(" \n");
+			// Eje z:
+			print_int(CONSOLE_UART, giro_z);
+			console_puts(" \n");
+			gpio_set(GPIOC, GPIO1);
+		}
+		else if (boton_com_enable == 0)
+		{
+			gfx_puts("OFF");
+			gpio_clear(GPIOG, GPIO13);
+		}
+		gfx_puts("Temperatura:");
+		lcd_show_frame();
 	}
+	return 0;
+}
